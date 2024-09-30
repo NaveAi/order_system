be7cb208832fe441a80cd2a56070f2865313957a
NaveAi <p8699322@gmail.com>
2024-09-30 14:13:35 +0300

    עדכון הגדרות לםני דיפלוי

--- orders_project/settings.py
+++ orders_project/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS')
 
 # Application definition
