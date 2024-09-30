6cbac7aaaf1d8e0627785af1c6d15d41738b38b6
NaveAi <p8699322@gmail.com>
2024-09-30 14:07:52 +0300

    עדכון הגדרות לםני דיפלוי

--- orders_project/settings.py
+++ orders_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f#^jr%75)e^6lri#+$qa9^ys!_t%9g0-0xkrx6ph!t^z3(gmqe'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
